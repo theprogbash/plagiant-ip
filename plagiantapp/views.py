b5e2c88444a7384ac1d508e1f1060c8971e07da5
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-10 23:10:25 +0400

    Test commit

--- plagiantapp/views.py
+++ plagiantapp/views.py
@@ -1,8 +1,8 @@
 from .models import Account, University
 from .forms import SignUpForm, SignInForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
     return render(request, 'index.html', {})
@@ -19,6 +19,7 @@ def sign_up(request):
     context['form'] = form
     return render(request, "sign_up.html", context)
 
+
 def sign_in(request):
     context = {}
     form = SignInForm(request.POST or None, request.FILES or None)
@@ -29,22 +30,6 @@ def sign_in(request):
         form = SignInForm(request.POST or None, request.FILES or None)
     context['form'] = form
     return render(request, "sign_in.html", context)
-
-#     if request.user.is_authenticated:
-#         return render(request, 'index.html')
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('/')
-#         else:
-#             form = AuthenticationForm(request.POST)
-#             return render(request, 'sign_in.html', {'form': form})
-#     else:
-#         form = AuthenticationForm()
-#         return render(request, 'sign_in.html', {'form': form})
 
 def sign_out(request):
     logout(request)
