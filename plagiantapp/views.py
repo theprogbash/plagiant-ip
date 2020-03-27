fcb1f62c4963e88aedd14d70f8ee1f9578724871
Kamran Mmmdv <amorfrap@gmail.com>
2020-03-27 19:02:48 +0400

    Removed unexpected indent

--- plagiantapp/views.py
+++ plagiantapp/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 import glob
 
 def index(request):
